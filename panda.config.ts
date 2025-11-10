c81c4070538e695a34f40fa683fa8a70976479d8
Amina <amefoon@gmail.com>
2025-11-10 01:48:22 -0500

    create design tokens
    * colors
    * typography

--- panda.config.ts
+++ panda.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from "@pandacss/dev";
+import {colors} from './theme/colors'
+import {typography, textStyles} from './theme/typography'
+import { defineGlobalStyles } from "@pandacss/dev";
+
+const globalCss = defineGlobalStyles({
+  'html, body': {
+    color: 'text',
+    lineHeight: '1.5',
+    fontSize: '16px',
+    fontFamily: 'var(--font-inter-sans)',
+  }
+})
 
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
+  globalCss,
 
   // Where to look for your css declarations
-  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}", './stories/**/*.{js,jsx,ts,tsx}'],
 
   // Files to exclude
   exclude: [],
@@ -13,7 +26,16 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+      tokens: {colors,
+        radii: {
+          sm: {value:'4px'},
+          md: {value:'8px'}
+        },
+        ...typography
+      },
+      extend: {
+        textStyles
+      }
   },
 
   // The output directory for your css system
